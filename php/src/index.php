a8069230ff60c5f19d7a6366962959cac6740358
Tobias Derksen <t.derksen@mailbox.org>
2022-02-18 16:51:59 +0100

    Initial php project

---
+++ php/src/index.php
@@ -0,0 +1,25 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\Factory\ServerRequestCreatorFactory;
+
+
+require __DIR__ . '/../vendor/autoload.php';
+
+AppFactory::setSlimHttpDecoratorsAutomaticDetection(false);
+ServerRequestCreatorFactory::setSlimHttpDecoratorsAutomaticDetection(false);
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Hello OpenShift");
+    return $response;
+});
+
+$app->get('/hello', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Hello OpenShift");
+    return $response;
+});
+
+$app->run();
