a838f68707961a31d8a8ec6eddaeaac68ad8da2d
Tobias Derksen <t.derksen@mailbox.org>
2022-02-21 20:09:07 +0100

    Fixed metrics middleware

--- src/metrics.php
+++ src/metrics.php
@@ -1,6 +1,5 @@
 <?php
 
-use Lukeelten\Php\TodoApp\MetricsMiddleware;
 use Prometheus\CollectorRegistry;
 use Prometheus\RenderTextFormat;
 use Prometheus\Storage\InMemory;
@@ -12,8 +11,20 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 function setupMetrics(App $app) {
     $registry = new CollectorRegistry(new InMemory());
 
-    $middleware = new MetricsMiddleware($registry);
-    $app->addMiddleware($middleware);
+    $counter = $registry->registerCounter("php_todo_app", "request", "requests to the php webserver", ["method", "scheme", "path", "code"]);
+
+    $app->add(function ($request, $response, $next) use ($counter) {
+        $uri = $request->getUri();
+        $scheme = $uri->getScheme();
+        $method = $request->getMethod();
+        $path = $uri->getPath();
+
+        $response = $next($request, $response);
+
+        $counter->inc([$method, $scheme, $path, $response->getStatusCode()]);
+
+        return $response;
+    });
 
     $app->get("/metrics", function (Request $request, Response $response, array $args) use ($registry) {
         $registry->getOrRegisterCounter("php_todo_app", "metrics", "How often is the metrics endpoint called")->inc();
